4f876c3acb5592f026a6cb627de9b15689d270ed
Shane Keulen <shane.keulen@gmail.com>
2016-06-15 14:42:12 -0400

    split up code, remove uneeded HOC in connectTheme

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
-import React, { Component, PropTypes, Children } from 'react';
+import { PropTypes, Children } from 'react';
 import omit from 'lodash.omit';
-import jss from 'jss';
-import isEqual from 'is-equal';
-
 import {
   compose,
   mapProps,
@@ -10,10 +7,10 @@ import {
   setDisplayName,
   wrapDisplayName,
   withContext,
-  branch,
-  lifecycle,
-  withProps
+  branch
 } from 'recompose';
+
+import withSheet from './with-sheet';
 
 const omitProp = propName => mapProps(props => omit(props, propName));
 
@@ -22,58 +19,9 @@ export const ThemeProvider = withContext(
   props => ({ theme: props.theme })
 )(props => Children.only(props.children));
 
-
-const withSheet = mapThemeToCss => {
-  let refs = 0;
-  let sheet = null;
-  let cache = null;
-
-  const attach = (nextState, force = false) => {
-    if (isEqual(nextState, cache)) return;
-
-    cache = nextState;
-    if (force || !sheet) sheet = jss.createStyleSheet(nextState);
-    sheet.attach();
-  };
-
-  const ref = (nextState) => {
-    if (refs === 0) attach(nextState);
-    refs++;
-    return sheet;
-  };
-
-  const deref = () => {
-    refs--;
-    if (refs === 0) {
-      sheet.detach();
-      cache = null;
-    }
-  };
-
-  return compose(
-    lifecycle({
-      componentWillMount() {
-        const nextState = mapThemeToCss(this.props.theme);
-        ref(nextState);
-      },
-      componentWillReceiveProps(nextProps) {
-        if (!isEqual(this.props.theme, nextProps.theme)) {
-          attach(mapThemeToCss(nextProps.theme), true);
-        }
-      },
-      componentWillUnmount() {
-        deref();
-      }
-    }),
-    withProps(() => ({
-      sheet
-    }))
-  );
-};
-
-export const connectTheme = mapThemeToCss => BaseComponent => {
-  return compose(
-    setDisplayName(wrapDisplayName(BaseComponent, 'gild')),
+export const connectTheme = mapThemeToCss =>
+  compose(
+    comp => setDisplayName(wrapDisplayName(comp, 'gild'))(comp),
     getContext({ theme: PropTypes.object }),
     branch(props => props.theme,
       compose(
@@ -82,7 +30,6 @@ export const connectTheme = mapThemeToCss => BaseComponent => {
         omitProp('sheet')
       ),
       c => c
-    ),
-  )(BaseComponent);
-};
+    )
+  );
 
---
+++ src/with-sheet.js
@@ -0,0 +1,56 @@
+import jss from 'jss';
+import isEqual from 'is-equal';
+
+import {
+  compose,
+  lifecycle,
+  withProps
+} from 'recompose';
+
+export const withSheet = mapThemeToCss => {
+  let refs = 0;
+  let sheet = null;
+  let cache = null;
+
+  const attach = (nextState, force = false) => {
+    if (isEqual(nextState, cache)) return;
+
+    cache = nextState;
+    if (force || !sheet) sheet = jss.createStyleSheet(nextState);
+    sheet.attach();
+  };
+
+  const ref = (nextState) => {
+    if (refs === 0) attach(nextState);
+    refs++;
+    return sheet;
+  };
+
+  const deref = () => {
+    refs--;
+    if (refs === 0) {
+      sheet.detach();
+      cache = null;
+    }
+  };
+
+  return compose(
+    lifecycle({
+      componentWillMount() {
+        const nextState = mapThemeToCss(this.props.theme);
+        ref(nextState);
+      },
+      componentWillReceiveProps(nextProps) {
+        if (!isEqual(this.props.theme, nextProps.theme)) {
+          attach(mapThemeToCss(nextProps.theme), true);
+        }
+      },
+      componentWillUnmount() {
+        deref();
+      }
+    }),
+    withProps(() => ({
+      sheet
+    }))
+  );
+};
